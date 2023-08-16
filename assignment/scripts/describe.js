e649259e089c3cbd3d515c71f71dfe82074fe29e
Chris Cantoni <chris.cantoni@gmail.com>
2023-08-16 14:24:53 -0500

    Complete problems with my solutions.

--- assignment/scripts/describe.js
+++ assignment/scripts/describe.js
@@ -26,9 +26,9 @@ if (number >= 2) {
 
 
 // 1. WRITE YOUR DESCRIPTION HERE
-// 
-// 
-// 
+// We make a variable called name and set it to the string 'Dane'.
+// We then check if variable name is set to 'Mary'. If it is, we would consolelog "Hi Mary!"
+// name is not set to 'Mary', it is set to 'Dane, so we console log 'How do you do?'
 //
 
 //CODE
@@ -45,10 +45,13 @@ if (name === 'Mary') {
 */
 
 // 2. WRITE YOUR DESCRIPTION HERE
-// 
-// 
-// 
-//
+// We make a variable called secret with no value.
+// We make a variable called code and set it to a value of 123.
+// We check if code equals 123. It does so we assign the variable secret
+// to a value of 'super.' We then multiply code by 2 so it equals 246.
+// We check if code is greater than 250, if it is we would assign secret the value of 'duper'
+// We then console log the value of secret, which is 'super'
+
 
 //CODE
 /*
@@ -70,10 +73,14 @@ console.log(secret)
 */
 
 // 3. WRITE YOUR DESCRIPTION HERE
-// 
-// 
-// 
-//
+// We have three variables, isStudent, age, and zip, set to values of true, 34 and 55407 respectively.
+// We then check if isStudent is true AND if zip is greater than 80000. If both are true, we would console log
+// 'You're a student on the West Coast!' 
+// The zip value is below 80000 so now we move to the next condition. If student is false and age is less than 30,
+// we would console log "What are your hobbies?"
+// Both of those are false, so we move to the next condition, which if student is true, then we console log
+// 'Welcome to Prime!'
+// If student had been false we'd move to the final condition and would console.log 'How about the weather?'
 
 //CODE
 /*
@@ -129,6 +136,10 @@ if (number >= 2) {
 // colorTwo is set to 'red', and mix is set to true. We check if mix is true 
 // -- it is, so we set colorOne and colorTwo to 'purple'
 
+//FIX - change the values of colorOne and colorTwo, as their values are mixed (heh).
+// Also mix being true or not isn't checking the values of colorOne and colorTwo so using it
+// as the conditional seems ineffective?
+
 /*
 let colorOne = 'red';
 let colorTwo = 'blue';
@@ -143,6 +154,8 @@ if (mix === true) {
 // We check if temp is higher than 39 and if time is greater or equal to  4 
 // -- they are so we console.log 'throw away the food!'
 
+// FIX - The conditional is testing OR instead of AND. Replace the || with &&
+
 /*
 let temp = 40;
 const time = 4;
@@ -155,6 +168,9 @@ if (temp > 39 || time >= 4) {
 //6. We start with two variables -- age is set to 21 and minAge is set to 21.
 // We check if age is greater than or equal to minAge 
 // -- it is, so we console.log 'enter'.
+
+// FIX -- we need to change the conditional statement to (age >= minAge). Right now it's
+// reversed.
 
 /*
 let age = 21;
